2a3e0c481586c9aefb7b1eeaaf4326a09a2f195d
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2020-04-03 02:32:43 -0500

    fixed many bugs

--- rosetta-code.hs
+++ rosetta-code.hs
@@ -1,23 +1,25 @@
 import Control.Monad.State.Lazy
 import Data.Char                        -- isAsciiLower, isAsciiUpper, isDigit
-import qualified Data.Text.Lazy as T
+import Data.Text.Lazy (Text)
+import qualified Data.Text as T
 import Text.Printf
+import System.Environment               -- getArgs
 import System.IO
 
-type Text = T.Text
 
 
 -- Scanning ------------------------------------------------------------------------------------------------------------
 -- Remaining text, line, column
 type ScannerState = (Text, Int, Int)
 
-
-advanceScanner :: ScannerState -> ScannerState
-advanceScanner (t, l, c)
+advanceScanner :: Int -> ScannerState -> ScannerState
+advanceScanner 1 (t, l, c)
     | ch == '\n' = (rest, l + 1, 0)
     | otherwise  = (rest, l, c + 1)
 
     where (ch, rest) = (T.head t, T.tail t)
+
+advanceScanner n (t, l, c) = advanceScanner (n - 1) $ advanceScanner 1
 
 
 peekScanner :: ScannerState -> Char
@@ -40,8 +42,8 @@ matchScanner lexeme (t, l, c) =
         Just t' -> (True, (t', l, c + (length lexeme)))
 
 
-grabWhileScanner :: (Char -> Bool) -> ScannerState -> (Text, ScannerState)
-grabWhileScanner f (t, l, c) = (str, (t', l, c + T.length str))
+manyScanner :: (Char -> Bool) -> ScannerState -> (Text, ScannerState)
+manyScanner f (t, l, c) = (str, (t', l, c + T.length str))
     where (str, t') = T.span f t
 
 
@@ -49,8 +51,8 @@ grabWhileScanner f (t, l, c) = (str, (t', l, c + T.length str))
 type Scanner = State ScannerState
 
 
-advance :: Scanner Char
-advance = modify advanceScanner
+advance :: Int -> Scanner ()
+advance n = modify $ advanceScanner n
 
 
 peek :: Scanner Char
@@ -65,8 +67,8 @@ match :: String -> Scanner Bool
 match lexeme = state $ matchScanner lexeme
 
 
-grabWhile :: (Char -> Bool) -> Scanner Text
-grabWhile f = state $ grabWhileScanner f
+many :: (Char -> Bool) -> Scanner Text
+many f = state $ manyScanner f
 
 
 -- Token ---------------------------------------------------------------------------------------------------------------
@@ -78,29 +80,28 @@ data Token = Token { tokenName   :: String,
                      tokenColumn :: Int }
 
 
-printToken :: Token -> IO ()
-printToken t = printf "%2d   %2d   %-17s%s\n" (tokenLine t) (tokenColumn t) (tokenName t) (tokenValue t)
+showToken :: Token -> String
+showToken t = printf "%2d   %2d   %-17s%s\n" (tokenLine t) (tokenColumn t) (tokenName t) (tokenValue t)
 
 
-printTokens :: [Token] -> IO ()
-printTokens tokens = do
-    print "Location     Token Name         Value\n" ++
-          "-------------------------------------\n"
-
-    map printToken tokens
+showTokens :: [Token] -> String
+showTokens tokens =
+    "Location     Token Name         Value\n" ++
+    "-------------------------------------\n" ++
+    concatMap showToken tokens
 
 
 -- Lexical Errors ------------------------------------------------------------------------------------------------------
 --          token context
-lexError :: ScannerState -> Scanner Token
+lexError :: ScannerState -> String -> Scanner Token
 lexError (t, l, c) msg = do
     (_, l', c') <- get    -- error context
 
-    let code = T.take (fromIntegral $ c' - c + 1) t
+    let code = T.take (c' - c + 1) t
     let error_str = printf "(%2d,%2d): %s" l' c' (T.unpack code)
 
-    advance
-    return Token "Error" (msg ++ "\n" ++ (replicate 13 ' ')  ++ error_str) l c
+    advance 1
+    return Token "Error" (msg ++ "\n" ++ (replicate 13 ' ') ++ error_str) l c
 
 
 -- Tokenizing ----------------------------------------------------------------------------------------------------------
@@ -115,9 +116,9 @@ isIdEnd ch = isIdStart ch || isDigit ch
 makeIdentifier :: Scanner Token
 makeIdentifier = do
     (text, line, column) <- get
-    advance
+    advance 1
 
-    back <- grabWhile isIdEnd
+    back <- many isIdEnd
     let id = T.cons (T.head text) back
 
     return Token "Identifier" id line column
@@ -127,13 +128,13 @@ makeInteger :: Scanner Token
 makeInteger = do
     ctx @ (_, line, column) <- get
 
-    int_str <- grabWhile isDigit
+    int_str <- many isDigit
     let num = read (T.unpack int_str) :: Integer
 
     ch <- peek
 
-    if (isIdStart $ ch) then
-        return lexError ctx "Invalid number. Starts like a number, but ends in non-numeric characters."
+    if (isIdStart ch) then
+        lexError ctx "Invalid number. Starts like a number, but ends in non-numeric characters."
     else
         return Token "Integer" num line column
 
@@ -143,25 +144,45 @@ makeString = do
     ctx @ (_, line, column) <- get
 
     let build_str s = do
+        (text, _, _) <- get
         ch <- peek
 
-        if (match "\"" ) then return s
+        if (ch == '"') then do
+            advance 1
+            return $ Token "String" s line column
 
         else if (ch == '\n') then
-            return lexError ctx "End-of-line while scanning string literal." ++
-                                " Closing string character not found before end-of-line."
+            lexError ctx $ "End-of-line while scanning string literal." ++
+                           " Closing string character not found before end-of-line."
 
-        else if (ch == "") then
-            return lexError ctx "End-of-file while scanning string literal." ++
-                                " Closing string character not found."
+        else if (text == T.pack "") then
+            lexError ctx $ "End-of-file while scanning string literal. Closing string character not found."
 
-        else
-            advance
-            return $ build_str (T.snoc s ch)
+        else do
+            advance 1
+            build_str (T.snoc s ch)
 
-    let str = build_str $ T.pack ""
+    return $ build_str (T.pack "")
 
-    return Token "String" str line column
+
+skipComment :: Scanner Token
+skipComment = do
+    ctx @ (t, l, s) <- get
+    let (c, rest) = T.breakOn (T.pack "*/" t)
+
+    if (T.length rest == 0) then
+        lexError ctx $ "End-of-file in comment. Closing comment characters not found."
+    else
+        advance $ T.length c
+        getToken
+
+
+-- Bespoke monadic choice
+-- (==>) :: Scanner Bool -> Scanner Token -> Scanner (Maybe Token)
+-- a @ (StateT ScannerState Identity cond) ==> StateT ScannerState Identity tok =
+--     if (cond)
+--         then a >>= \_ -> return Just tok
+--         else a >>= \_ -> return Nothing
 
 
 getToken :: Scanner Token
@@ -172,83 +193,88 @@ getToken = do
     ch <- peek
 
     -- Keywords
-    if      (match "if"   ) then return Token "Keyword_if"    None line column
-    else if (match "else" ) then return Token "Keyword_else"  None line column
-    else if (match "while") then return Token "Keyword_while" None line column
-    else if (match "print") then return Token "Keyword_print" None line column
-    else if (match "putc" ) then return Token "Keyword_putc"  None line column
+    if      (T.isPrefixOf "if"    text) then do advance 2; return $ Token "Keyword_if"    None line column
+    else if (T.isPrefixOf "else"  text) then do advance 4; return $ Token "Keyword_else"  None line column
+    else if (T.isPrefixOf "while" text) then do advance 5; return $ Token "Keyword_while" None line column
+    else if (T.isPrefixOf "print" text) then do advance 5; return $ Token "Keyword_print" None line column
+    else if (T.isPrefixOf "putc"  text) then do advance 4; return $ Token "Keyword_putc"  None line column
 
     -- End of Input
     else if (match "") then return Token "EOF" None line column
 
     -- Patterns
-    else if (isIdStart $ ch) then return makeIdentifier
-    else if (isDigit $ ch)   then return makeInteger
-    else if (match "\"")     then return makeString
-    else if (match "/*")     then return skipComment
+    else if (isIdStart $ ch) then makeIdentifier
+    else if (isDigit $ ch)   then makeInteger
+    else if (match "\"")     then makeString
+    else if (match "/*")     then skipComment
 
     -- Operators
-    else if (match "*" ) then return Token "Op_multiply"     None line column
-    else if (match "/" ) then return Token "Op_divide"       None line column
-    else if (match "%" ) then return Token "Op_mod"          None line column
-    else if (match "+" ) then return Token "Op_add"          None line column
-    else if (match "-" ) then return Token "Op_subtract"     None line column
-    else if (match "<=") then return Token "Op_lessequal"    None line column
-    else if (match "<" ) then return Token "Op_less"         None line column
-    else if (match ">=") then return Token "Op_greaterequal" None line column
-    else if (match ">" ) then return Token "Op_greater"      None line column
-    else if (match "==") then return Token "Op_equal"        None line column
-    else if (match "!=") then return Token "Op_notequal"     None line column
-    else if (match "!" ) then return Token "Op_not"          None line column
-    else if (match "=" ) then return Token "Op_assign"       None line column
-    else if (match "&&") then return Token "Op_and"          None line column
-    else if (match "||") then return Token "Op_or"           None line column
+    else if (match "*" ) then return $ Token "Op_multiply"     None line column
+    else if (match "/" ) then return $ Token "Op_divide"       None line column
+    else if (match "%" ) then return $ Token "Op_mod"          None line column
+    else if (match "+" ) then return $ Token "Op_add"          None line column
+    else if (match "-" ) then return $ Token "Op_subtract"     None line column
+    else if (match "<=") then return $ Token "Op_lessequal"    None line column
+    else if (match "<" ) then return $ Token "Op_less"         None line column
+    else if (match ">=") then return $ Token "Op_greaterequal" None line column
+    else if (match ">" ) then return $ Token "Op_greater"      None line column
+    else if (match "==") then return $ Token "Op_equal"        None line column
+    else if (match "!=") then return $ Token "Op_notequal"     None line column
+    else if (match "!" ) then return $ Token "Op_not"          None line column
+    else if (match "=" ) then return $ Token "Op_assign"       None line column
+    else if (match "&&") then return $ Token "Op_and"          None line column
+    else if (match "||") then return $ Token "Op_or"           None line column
 
     -- Symbols
-    else if (match "(") then return Token "LeftParen"  None line column
-    else if (match ")") then return Token "RightParen" None line column
-    else if (match "{") then return Token "LeftBrace"  None line column
-    else if (match "}") then return Token "RightBrace" None line column
-    else if (match ";") then return Token "SemiColon"  None line column
-    else if (match ",") then return Token "Comma"      None line column
+    else if (match "(") then return $ Token "LeftParen"  None line column
+    else if (match ")") then return $ Token "RightParen" None line column
+    else if (match "{") then return $ Token "LeftBrace"  None line column
+    else if (match "}") then return $ Token "RightBrace" None line column
+    else if (match ";") then return $ Token "SemiColon"  None line column
+    else if (match ",") then return $ Token "Comma"      None line column
 
     -- Bad Input
-    else return lexError ctx "Unrecognized character."
+    else lexError ctx "Unrecognized character."
 
 
-getTokens :: Scanner [Token]
-getTokens = do
-    t <- getToken
+getTokens :: ScannerState -> [Token]
+getTokens s
+    | tokenName t == "EOF" = [t]
+    | otherwise            = t : getTokens s'
 
-    if (tokenName t == "EOF") then return t
-    else                           return (t : getTokens)
+    where (t, s') = runState getToken s
 
 
 tokenize :: String -> [Token]
-tokenize s = runState getTokens (T.pack s, 0, 0)
+tokenize s = getTokens (T.pack s, 0, 0)
 
 
 
 -- Run -----------------------------------------------------------------------------------------------------------------
 getIOHandles :: [String] -> IO (Handle, Handle)
-getIOHandles [] = (stdin, stdout)
+getIOHandles [] = return (stdin, stdout)
 
 getIOHandles [infile] = do
-    inhandle <- openfile infile ReadMode
+    inhandle <- openFile infile ReadMode
     return (inhandle, stdout)
 
 getIOHandles [infile, outfile] = do
-    inhandle  <- openfile infile ReadMode
-    outhandle <- openfile outfile WriteMode
+    inhandle  <- openFile infile ReadMode
+    outhandle <- openFile outfile WriteMode
     return (inhandle, outhandle)
+
+
+parseTokens :: Handle -> Handle -> IO ()
+parseTokens in_handle out_handle = do
+    contents <- hGetContents in_handle
+    hPutStr out_handle $ showTokens (tokenize contents)
 
 
 main = do
     args <- getArgs
-    let (inhandle, outhandle) = getIOHandles args
-    contents <- hGetContents inhandle
+    (in_handle, out_handle) <- getIOHandles args
 
-    map printToken $ tokenize contents
+    parseTokens in_handle out_handle
 
-    hclose inhandle
-    hclose outhandle
+    hClose in_handle
+    hClose out_handle
